11e31d267029f8b33a9840209335b8e572fce40a
Taras Mankovski <tarasm@gmail.com>
2013-01-05 22:21:45 +0100

    Initial commit

---
+++ classes/class-theme-base.php
@@ -0,0 +1,30 @@
+<?php
+if ( !class_exists('WRKTG_Public_Theme') ) {
+
+    class WRKTG_Public_Theme extends ScaleUp_Theme {
+
+        function __construct() {
+
+            add_action( 'wp_footer', array( $this, 'wp_footer') );
+
+        }
+
+        function wp_footer() {
+            /**
+             * @todo: This should be loaded differently. Not sure how yet.
+             */
+            ?>
+            <script type="text/javascript">
+                jQuery(document).ready(function($) {
+                    contact = $('.contact');
+                    contact.find('.gform_button').addClass('btn').addClass('span12');
+                    contact.find('#field_1_1, #field_1_2').addClass('span6');
+                    contact.find('#input_1_1, #input_1_2').addClass('span12');
+                    contact.find('h3').prepend($('<i class="icon icon-envelope-alt"></i>'));
+                });
+            </script>
+        <? }
+
+    }
+
+}
