b76b395794459af1237daa9289fcdb5126975053
Taras Mankovski <tarasm@gmail.com>
2013-01-23 23:39:35 +0100

    Removed uncessary templates

--- one-page-article.php
+++
@@ -1,9 +0,0 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-    <div class="entry-content clearfix">
-        <?php
-        the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'the-bootstrap' ) );
-        the_bootstrap_link_pages(); ?>
-    </div><!-- .entry-content -->
-
-</article><!-- #post-<?php the_ID(); ?> -->
--- one-page.php
+++
@@ -1,5 +0,0 @@
-<?php if ( $subpages = get_field('pages') ):  ?>
-    <?php foreach ( $subpages as $post ) : setup_postdata( $post ) ?>
-        <?php get_template_part( 'one-page-article' ) ?>
-    <?php endforeach; ?>
-<?php endif ?>
