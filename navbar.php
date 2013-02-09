94454d7c4cb84807365c2b1c177dcf624cbac9b5
Taras Mankovski <tarasm@gmail.com>
2013-02-09 17:43:27 +0100

    Added How We Work to top menu

--- navbar.php
+++ navbar.php
@@ -5,6 +5,7 @@
       <a class="brand" href="#">WRKTG</a>
       <ul class="nav">
         <li class="active"><a href="#primary">Home</a></li>
+        <li><a href="#how-we-work">How We Work</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
     </div>
